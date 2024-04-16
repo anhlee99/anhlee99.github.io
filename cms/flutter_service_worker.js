97459307e5bec5bac140fdf5a9941cd60a7c5444
Adam <anhle99.work@gmail.com>
2024-04-16 13:17:56 +0700

    d

--- cms/flutter_service_worker.js
+++ cms/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "89edf1187aa4a10cb511769ee017e261",
-"index.html": "a90483fd850f0a90ac874d166af5a832",
-"/": "a90483fd850f0a90ac874d166af5a832",
-"main.dart.js": "e5ff4b72abc400c9a22bd87320c4f091",
+"index.html": "35920b1ca4095066c82b9e7d648930f4",
+"/": "35920b1ca4095066c82b9e7d648930f4",
+"main.dart.js": "e19996b566ffa03955046e39522b1464",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
