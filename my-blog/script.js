be1b61965b3da91d2b6f301cbb88de98488c8a81
Adam <anhle99.work@gmail.com>
2024-07-23 23:19:08 +0700

    [new Project]

---
+++ my-blog/script.js
@@ -0,0 +1,36 @@
+
+
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingLight }) {
+    if (localStorageTheme !== null) {
+        return localStorageTheme;
+    }
+
+    if (systemSettingLight.matches) {
+        return "dark";
+    }
+
+    return "light";
+}
+
+const localStorageTheme = localStorage.getItem("mode");
+const systemSettingLight = window.matchMedia("(prefers-color-scheme: light)");
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingLight });
+console.log(currentThemeSetting);
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+
+function darkMode() {
+    localStorage.setItem("mode", "darkmode" === localStorage.getItem("mode") ? "light" : "darkmode");
+    updateThemeOnHtmlEl({ theme: localStorage.getItem("mode") });
+};
+
+function updateThemeOnHtmlEl({ theme }) {
+    document.querySelector("html").setAttribute("data-theme", theme);
+    if (theme === "darkmode") {
+        document.getElementsByClassName("theme-isDark")[0].children[0].setAttribute("class", "bx bx-sun");
+    }
+    else {
+        document.getElementsByClassName("theme-isDark")[0].children[0].setAttribute("class", "bx bx-moon");
+    }
+}
+
